1befad94a2a064df1a64dcbc9646b8e0ea71de31
Istvan <istvan.balogh@outlook.com>
2021-12-03 13:47:33 +0700

    Rename view controller

--- FLITChart/WealthCalculator/InvestmenReturnsViewController.swift
+++ FLITChart/WealthCalculator/InvestmenReturnsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class InvestmenReturnsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
