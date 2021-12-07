81ef46795449b4c514b9c9604b37aa7483e9f6a3
Istvan <istvan.balogh@outlook.com>
2021-12-07 11:47:16 +0700

    Add snapshot tests

--- FLITChartTests/InvestmentReturns UI/InvestmenReturnsSnapshotTests.swift
+++ FLITChartTests/InvestmentReturns UI/InvestmenReturnsSnapshotTests.swift
@@ -15,9 +15,9 @@ class InvestmenReturnsSnapshotTests: XCTestCase {
 
 //        sut.display(feedWithContent())
 
-        record(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "INVESTMENTRETURNS_WITH_CONTENT_light")
-        record(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "INVESTMENTRETURNS_WITH_CONTENT_dark")
-        record(snapshot: sut.snapshot(for: .iPhone13(style: .light, contentSize: .extraExtraExtraLarge)), named: "INVESTMENTRETURNS_WITH_CONTENT_light_extraExtraExtraLarge")
+        assert(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "INVESTMENTRETURNS_WITH_CONTENT_light")
+        assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "INVESTMENTRETURNS_WITH_CONTENT_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone13(style: .light, contentSize: .extraExtraExtraLarge)), named: "INVESTMENTRETURNS_WITH_CONTENT_light_extraExtraExtraLarge")
     }
     
     private func makeSUT() -> InvestmenReturnsViewController {
