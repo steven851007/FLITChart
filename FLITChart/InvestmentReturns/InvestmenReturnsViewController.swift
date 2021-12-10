0e3188ce699dade24ecd28f623434bbfcb590b7b
Istvan <istvan.balogh@outlook.com>
2021-12-10 13:50:24 +0700

    Add gradient background

--- FLITChart/InvestmentReturns/InvestmenReturnsViewController.swift
+++ FLITChart/InvestmentReturns/InvestmenReturnsViewController.swift
@@ -7,6 +7,35 @@
 
 import UIKit
 
+class ChartGradientView: UIView {
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setGradientBackground(colorTop: .systemBlue, colorBottom: .systemBackground)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setGradientBackground(colorTop: .systemBlue, colorBottom: .systemBackground)
+    }
+    
+    private var gradientLayer = CAGradientLayer()
+    
+    private func setGradientBackground(colorTop: UIColor, colorBottom: UIColor){
+            gradientLayer.colors = [colorTop.cgColor, colorBottom.cgColor]
+            gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+            gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+            gradientLayer.locations = [0, 1]
+            gradientLayer.frame = bounds
+
+            layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+}
+
 public class InvestmenReturnsViewController: UIViewController {
 
     @IBOutlet weak var initialInvestmentTextField: UITextField!
